6ae8327016141270b2fc5580588681e3b61730e0
Harry Reeder <harry@harryreeder.co.uk>
2015-04-04 17:47:34 +0100

    Updated Model for mailgun standards

--- grum/api/models/message.py
+++ grum/api/models/message.py
@@ -2,10 +2,18 @@ from grum import db
 
 
 class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
-    to = db.Column(db.String(64))
-    cc = db.Column(db.String(64))
+    id = db.Column(db.String(128), primary_key=True)
 
-    subject = db.Column(db.String(256))
-    body = db.Column(db.Text)
+    recipient = db.Column(db.String(128))
+    recipient_nice = db.Column(db.String(128))
+
+    sender = db.Column(db.String(128))
+    sender_nice = db.Column(db.String(128))
+    sent_at = db.Column(db.Timestamp)
+
+    html = db.Column(db.Text)
+    html_stripped = db.Column(db.Text)
+
+    plaintext = db.Column(db.Text)
+    plaintext_stripped = db.Column(db.Text)
+    plaintext_stripped_signature = db.Column(db.Text)
