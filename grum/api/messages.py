3feeddcf34928e9c1bca4c9de0f5028686085c25
Harry Reeder <harry@harryreeder.co.uk>
2015-04-04 21:35:35 +0100

    Update messages API

--- grum/api/messages.py
+++ grum/api/messages.py
@@ -1,6 +1,20 @@
+from flask import jsonify
 from flask.ext.restful import Resource
+from .models import Message
 
 
 class Messages(Resource):
-    def get(self):
-        return "hello friend"
+    def get(self, message_id):
+        msg = Message.query.filter_by(id=message_id).first_or_404()
+        return jsonify(message={
+            'id': msg.id,
+            'from': msg.sender,
+            'from_nice': msg.sender_nice,
+            'timestamp': msg.sent_at,
+            'subject': msg.subject,
+            'html': msg.html,
+            'html_stripped': msg.html_stripped,
+            'plaintext': msg.plaintext,
+            'plaintext_stripped': msg.plaintext_stripped,
+            'plaintext_stripped_signature': msg.plaintext_stripped_signature
+        })
