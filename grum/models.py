086ea010a60d30cdf6e7838a1be1618c2107fc71
Harry Reeder <harry@harryreeder.co.uk>
2015-04-04 15:36:39 +0100

    Update User Model, Add Password Hashing

--- grum/models.py
+++ grum/models.py
@@ -1,3 +1,4 @@
+import bcrypt
 from grum import db
 
 
@@ -5,3 +6,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(128))
+
+    def __init__(self, username=None, password=None):
+        if username:
+            self.username = username
+        if password:
+            self.set_password(password)
+
+    def set_password(self, plaintext_password):
+        self.password = bcrypt.hashpw(plaintext_password.encode('utf-8'), bcrypt.gensalt())
+
+    def validate_password(self, plaintext_password):
+        hashed = bcrypt.hashpw(plaintext_password.encode('utf-8'), bytes(self.password.encode('utf-8')))
+        return hashed == bytes(self.password.encode('utf-8'))
