a7a6327660ed482f42e69c2c4ec5d5d9dc9b1699
Harry Reeder <harry@harryreeder.co.uk>
2015-04-04 15:12:08 +0100

    Add User class

--- grum/api/__init__.py
+++ grum/api/__init__.py
@@ -1,3 +1,6 @@
 from flask import Blueprint
+from flask.ext.restful import Api
+
 
 api = Blueprint("api", __name__, template_folder="templates")
+rest = Api(api)
--- grum/models.py
+++ grum/models.py
@@ -1,1 +1,7 @@
-__author__ = 'skull'
+from grum import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    password = db.Column(db.String(128))
