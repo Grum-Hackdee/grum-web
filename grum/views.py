a6ab193c6b75fa2f1536662f13e102ea6f484a8b
Hamish Hutchings <hamhut1066@gmail.com>
2015-04-04 15:37:50 +0100

    Add base html pages for registration

--- grum/views.py
+++ grum/views.py
@@ -1,6 +1,12 @@
 from grum import app
 from flask import render_template
 
+
 @app.route("/")
 def main():
     return render_template("index.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
