0c6ee19e3ee273216452aa8e04a08768fc70414b
Harry Reeder <harry@harryreeder.co.uk>
2015-04-05 09:45:57 +0100

    Add sentry

--- grum/__init__.py
+++ grum/__init__.py
@@ -2,11 +2,18 @@ import os
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
 if "DEBUG" in os.environ:
     app.config['DEBUG'] = True
     app.debug=True
+
+if "SENTRY_DSN" in os.environ:
+    app.config['SENTRY_DSN'] = os.environ['SENTRY_DSN']
+
+    from raven.contrib.flask import Sentry
+    sentry = Sentry(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 
 db = SQLAlchemy(app)
