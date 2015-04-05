3045e3ce1d952bb6c88d7c30e91510c3fae23c41
Harry Reeder <harry@harryreeder.co.uk>
2015-04-05 05:53:55 +0100

    Begin Email Account Info

--- grum/models.py
+++ grum/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(128))
+    display_name = db.Column(db.String(128))
 
     def __init__(self, username=None, password=None):
         if username:
@@ -19,3 +20,9 @@ class User(db.Model):
     def validate_password(self, plaintext_password):
         hashed = bcrypt.hashpw(plaintext_password.encode('utf-8'), bytes(self.password.encode('utf-8')))
         return hashed == bytes(self.password.encode('utf-8'))
+
+
+class EmailAccount(db.Model):
+    address = db.Column(db.String(128), primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    mg_api = db.Column(db.String(64))
